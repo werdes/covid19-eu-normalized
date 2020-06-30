cfeb2fd1494195ade003331d22ac5da071ad90f5
Werdes <marius_noack@yahoo.de>
2020-06-30 16:59:50 +0200

    population data scaling

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function () {
     load();
 });
 
-$('input[name=source], input[name=source-value], input[name=scaling], input[name=sorting]').change(function () {
+$('input[name=source], input[name=source-value], input[name=source-scaling], input[name=scaling], input[name=sorting]').change(function () {
     load();
 });
 
@@ -24,6 +24,7 @@ function load() {
     $('#threshold-deaths').text($('#field-threshold-deaths').val());
     $('#days-before-threshold').text($('#field-days-before-threshold').val());
 
+    setHideButton('hide');
     switch ($('input[name=source]:checked').val()) {
         case 'johns-hopkins-csse-confirmed':
             loadJHCSSEConfirmed(displayData);
@@ -41,6 +42,7 @@ function displayData(regions, lowestDate, lowestThreshold, dataType) {
     var daysBeforeThreshold = parseInt($('#field-days-before-threshold').val());
     var sorting = $('input[type=radio][name=sorting]:checked').val();
     var sourceValue = $('input[type=radio][name=source-value]:checked').val();
+    var sourceScaling = $('input[type=radio][name=source-scaling]:checked').val();
 
     minDate = lowestDate.subtract(daysBeforeThreshold, 'days');
     console.log(lowestDate + " -> " + minDate);
@@ -65,10 +67,21 @@ function displayData(regions, lowestDate, lowestThreshold, dataType) {
             Object.keys(region.byDay).forEach(function (day) {
                 var dayObj = moment(day, "YYYY/MM/DD");
                 var daysSinceStart = moment.duration(dayObj.diff(region.reachedThresholdAt, 'days'));
-                var dateCorrected = dayObj.subtract(region.dayDiffToLowest, "days");
+                var dateCorrected = moment(day, "YYYY/MM/DD").subtract(region.dayDiffToLowest, "days");
 
-                var addedFromLast = lastValue == null ? null : region.byDay[day] - lastValue;
 
+                var currentValue;
+                var addedFromLast;
+                if (sourceScaling == 'relative') {
+
+                    addedFromLast = lastValue == null ? null : region.byDayRelative[day] - lastValue;
+                    currentValue = region.byDayRelative[day];
+                    if (sourceValue == 'new') currentValue = addedFromLast;
+                } else {
+                    addedFromLast = lastValue == null ? null : region.byDay[day] - lastValue;
+                    currentValue = region.byDay[day];
+                    if (sourceValue == 'new') currentValue = addedFromLast;
+                }
                 if (dateCorrected >= minDate) {
                     points.push({
                         x: dateCorrected.toDate(),
@@ -76,13 +89,16 @@ function displayData(regions, lowestDate, lowestThreshold, dataType) {
                         daysSinceStart: daysSinceStart,
                         region: region,
                         dataType: dataType,
-                        y: sourceValue == "total" ? region.byDay[day] : addedFromLast,
+                        y: currentValue,
                         addedFromLast: addedFromLast,
+                        sourceScaling: sourceScaling,
                         total: region.byDay[day]
                     });
                 }
 
-                lastValue = region.byDay[day];
+                lastValue = sourceScaling == 'relative' ?
+                    region.byDayRelative[day] : region.byDay[day];
+
             });
 
             series.push({
@@ -114,93 +130,122 @@ function loadJHCSSE(cb, url, dataType, threshold) {
     var lowestThreshold = null;
     var dateRegex = "([0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,2})\\w+";
     var regions = {};
+    var sourceScaling = $('input[type=radio][name=source-scaling]:checked').val();
 
     $.get({
-        url: url,
+        url: "data/un_populationdata.csv",
         cache: true,
-        complete: function (data) {
-            var cellData = csvToArray(data.responseText);
-            var dateIdxs = {};
+        complete: function (populationData) {
+            populationData = csvToArray(populationData.responseText, ';');
+            populationData.shift();
 
-            var headlineCells = cellData[0];
-
-            headlineCells.forEach(function (headlineCell, headlineCellIdx) {
-                if (headlineCell.match(dateRegex)) {
-                    var date = moment(headlineCell);
-                    var dateFormatted = date.format("YYYY/MM/DD");
-                    dateIdxs[dateFormatted] = headlineCellIdx;
-                }
+            var populationByCountry = {};
+            populationData.forEach(function (row) {
+                populationByCountry[row[0]] = parseInt(row[1]);
             });
 
+            $('body').append('<textarea>' + JSON.stringify(populationByCountry) + '</textarea>');
 
-            cellData.shift();
-            cellData.forEach(function (cells, idx) {
+            $.get({
+                url: url,
+                cache: true,
+                complete: function (data) {
+                    var cellData = csvToArray(data.responseText);
+                    var dateIdxs = {};
 
-                var countryName = cells[1];
-                if (countryName != undefined) {
-                    if (!regions[countryName]) {
-                        regions[countryName] = {
-                            country: countryName,
-                            reachedThresholdAt: null,
-                            reachedThreshold: false,
-                            dayDiffToLowest: 0,
-                            fullCaseCount: 0,
-                            byDay: {}
+                    var headlineCells = cellData[0];
+
+                    headlineCells.forEach(function (headlineCell, headlineCellIdx) {
+                        if (headlineCell.match(dateRegex)) {
+                            var date = moment(headlineCell);
+                            var dateFormatted = date.format("YYYY/MM/DD");
+                            dateIdxs[dateFormatted] = headlineCellIdx;
                         }
-                    }
-
-                    var region = regions[countryName];
-
-                    // if (region.country != "France") return;
-
-                    Object.keys(dateIdxs).forEach(function (date) {
-                        var lineDateIdx = dateIdxs[date];
-                        var value = parseInt(cells[lineDateIdx]);
-
-                        if (!region.byDay[date])
-                            region.byDay[date] = 0;
-
-                        region.byDay[date] += value;
                     });
+
+
+                    cellData.shift();
+                    cellData.forEach(function (cells, idx) {
+
+                        var countryName = cells[1];
+                        if (countryName != undefined) {
+                            if (!regions[countryName]) {
+                                var population = null;
+                                if (populationByCountry[countryName]) {
+                                    population = populationByCountry[countryName];
+                                }
+
+                                regions[countryName] = {
+                                    country: countryName,
+                                    reachedThresholdAt: null,
+                                    reachedThreshold: false,
+                                    dayDiffToLowest: 0,
+                                    fullCaseCount: 0,
+                                    population: population,
+                                    byDay: {},
+                                    byDayRelative: {}
+                                }
+                            }
+
+                            var region = regions[countryName];
+
+                            // if (region.country != "France") return;
+
+                            Object.keys(dateIdxs).forEach(function (date) {
+                                var lineDateIdx = dateIdxs[date];
+                                var value = parseInt(cells[lineDateIdx]);
+
+                                if (!region.byDay[date])
+                                    region.byDay[date] = 0;
+
+                                region.byDay[date] += value;
+                            });
+                        }
+                    });
+
+                    Object.keys(regions).forEach(function (countryName) {
+                        var region = regions[countryName];
+                        Object.keys(region.byDay).forEach(function (date) {
+                            region.byDayRelative[date] = (region.byDay[date] / region.population) * 100000
+                        });
+                    });
+
+                    Object.keys(regions).forEach(function (countryName) {
+                        var region = regions[countryName];
+                        Object.keys(region.byDay).forEach(function (date) {
+                            var dateObj = moment(date, "YYYY/MM/DD");
+
+                            if (region.byDay[date] >= threshold) {
+                                if (!region.reachedThreshold) {
+                                    region.reachedThresholdAt = dateObj;
+                                    console.log(region.country + " reached threshold " + threshold + " with " + region.byDay[date] + " at " + dateObj);
+                                }
+                                region.reachedThreshold = true;
+
+                                if (region.byDay[date] > region.fullCaseCount) {
+                                    region.fullCaseCount = region.byDay[date];
+                                }
+                                if (lowestThreshold == null ||
+                                    lowestThreshold > dateObj) {
+                                    lowestThreshold = dateObj;
+                                }
+
+                            }
+                            if (lowestDate == null ||
+                                lowestDate > dateObj) {
+                                lowestDate = dateObj;
+                            }
+                        });
+                    });
+
+                    Object.keys(regions).forEach(function (country) {
+                        var region = regions[country];
+                        region.dayDiffToLowest = Math.round((region.reachedThresholdAt - lowestDate) / (1000 * 60 * 60 * 24));
+                    });
+                    $('body').append('<textarea>' + JSON.stringify(regions) + '</textarea>');
+                    cb(regions, lowestDate, lowestThreshold, dataType);
                 }
             });
-
-            Object.keys(regions).forEach(function (countryName) {
-                var region = regions[countryName];
-                Object.keys(region.byDay).forEach(function (date) {
-                    var dateObj = moment(date, "YYYY/MM/DD");
-
-                    if (region.byDay[date] >= threshold) {
-                        if (!region.reachedThreshold) {
-                            region.reachedThresholdAt = dateObj;
-                            console.log(region.country + " reached threshold " + threshold + " with " + region.byDay[date] + " at " + dateObj);
-                        }
-                        region.reachedThreshold = true;
-
-                        if (region.byDay[date] > region.fullCaseCount) {
-                            region.fullCaseCount = region.byDay[date];
-                        }
-                        if (lowestThreshold == null ||
-                            lowestThreshold > dateObj) {
-                            lowestThreshold = dateObj;
-                        }
-
-                    }
-                    if (lowestDate == null ||
-                        lowestDate > dateObj) {
-                        lowestDate = dateObj;
-                    }
-                });
-            });
-
-            Object.keys(regions).forEach(function (country) {
-                var region = regions[country];
-                region.dayDiffToLowest = Math.round((region.reachedThresholdAt - lowestDate) / (1000 * 60 * 60 * 24));
-            });
-
-            $('body').append('<textarea>' + JSON.stringify(regions) + '</textarea>');
-
-            cb(regions, lowestDate, lowestThreshold, dataType);
         }
     });
 }
@@ -210,19 +255,21 @@ Highcharts.Point.prototype.tooltipFormatter = function (useHeader) {
     var dataType = point.dataType == "cases" ? "confirmed cases" : "deaths";
     var addedFromLast = point.addedFromLast == null ? "" : "new " + dataType + ": " + point.addedFromLast + "<br />";
     var realDate = point.real.format("YYYY/MM/DD");
-    return "<b>" + point.region.country + " </b> | Day " + point.daysSinceStart + "<br/>" + point.total + " " + dataType + "<br />" + addedFromLast + "Offset: " + point.region.dayDiffToLowest + " days<br/>" + realDate;
+    var scalingHint = point.sourceScaling == "relative" ? (" per 100 000 inhabitants<br>Total: " + point.total + " " + dataType + "<br>Population: " + point.region.population) : "";
+    return "<b>" + point.region.country + " </b> | Day " + point.daysSinceStart + "<br/>" + point.y.toFixed(0) + " " + dataType + scalingHint + "<br />" + addedFromLast + "Offset: " + point.region.dayDiffToLowest + " days<br/>" + realDate;
 }
 
 // Return array of string values, or NULL if CSV string not well formed.
-function csvToArray(text) {
-    var re_value = /(".*?"|[^",]+)(?=\s*,|\s*$)/g;
+function csvToArray(text, separator = ',') {
+    var re = '(".*?"|[^"' + separator + ']+)(?=\s*' + separator + '|\s*$)';
+    var re_value = new RegExp(re, "g");
     // Return NULL if input string is not well formed CSV string.
     var r = []; // Initialize array to receive values.
     var lines = text.replace('\r', '').split('\n');
 
     lines.forEach(function (line) {
         var a = [];
-        if (line[0] == ',') a.push('');
+        if (line[0] == separator) a.push('');
         line.replace(re_value, // "Walk" the string using replace with callback.
             function (m0, m1, m2, m3) {
                 // Remove backslash from \' in single quoted values.
@@ -233,7 +280,7 @@ function csvToArray(text) {
                 return ''; // Return empty string.
             });
         // Handle special case of empty last value.
-        if (/,\s*$/.test(text)) a.push('');
+        if (new RegExp(separator + '\s*$').test(text)) a.push('');
 
         r.push(a);
     });
@@ -243,26 +290,38 @@ function csvToArray(text) {
 
 $('#toggle-series').click(function () {
     var status = $(this).attr('data-hide');
+    setChartHiddenStatus(status);
+});
+
+function setHideButton(status) {
+    if (status == "show") {
+        $(this).attr('data-hide', 'show');
+        $(this).text('Show all');
+    } else {
+
+        $(this).attr('data-hide', 'hide');
+        $(this).text('Hide all');
+    }
+}
+
+function setChartHiddenStatus(status) {
     var chart = $('#chart').highcharts();
     if (status == "hide") {
         $.each(chart.series, function (i, series) {
             console.log(series.name);
             series.setVisible(false, false);
         });
-
-        $(this).attr('data-hide', 'show');
-        $(this).text('Show all');
+        setHideButton('show');
     } else {
         $.each(chart.series, function (i, series) {
             console.log(series.name);
             series.setVisible(true, false);
         });
 
-        $(this).attr('data-hide', 'hide');
-        $(this).text('Hide all');
+        setHideButton('hide');
     }
     chart.redraw();
-})
+}
 
 function getChartOptions(lowestDate, series, dataType) {
     var scaling = $('input[type=radio][name=scaling]:checked').val();
